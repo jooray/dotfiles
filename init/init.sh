d73819a0c543987e9baa60154ce36b749b6c7ce4
Juraj Bednar <juraj@bednar.io>
2021-11-29 16:55:05 +0100

    update init script

--- init/init.sh
+++ init/init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-~/init/install-janus.sh
-
-pip install --user powerline-status
-pip install segno # for QR codes using qr function
-
 if [ $(uname) == "Darwin" ];then
 		~/init/macos
 		~/init/brew.sh
 		~/init/brew-cask.sh
     ~/init/macos-notifications
 	fi
+
+pip install --user powerline-status
+pip install segno # for QR codes using qr function
+
